9b0e47c4845bbbdb73340d02f8e24b01cd65755c
nihar8262 <niharnics@gmail.com>
2024-01-11 10:47:12 +0530

    updated

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,20 +14,20 @@ export const Project = () => {
 
       {/* Project header */}
       <div className="text-white text-center mt-10  w-[20rem] md:w-[35rem]">
-        <h1 className="font-bold text-3xl mb-10 border-b-2 border-b-yellow-500 py-2">Projects</h1>
+        <h1 className="font-bold text-3xl mb-16 border-b-2 border-b-yellow-500 py-2">Projects</h1>
         <p className="">
         I have worked on a wide range of projects. 
         From web apps to android apps. Here are some of my projects.
         </p>
       </div>
       {/* Option */}
-      <div className='flex gap-4 text-[#854CE6] p-1  mt-16 border border-[#854CE6] rounded-xl items-center justify-center bg-slate-900 md:text-xl'>
+      <div className='flex gap-4 text-[#854CE6] p-1  mt-20 border border-[#854CE6] rounded-xl items-center justify-center bg-slate-900 md:text-xl'>
         
         <button  className=' p-1 border-[#854CE6] hover:text-yellow-500'>ALL</button>
         <button  className='border-l p-1 border-[#854CE6] hover:text-yellow-500'>WEB APP'S</button>
         <button className='border-l p-1 border-[#854CE6] hover:text-yellow-500'>ANDROID APP'S</button>
       </div>
-      <div className=" absolute top-52 left-20 md:top-52 md:left-96  transfom transition duration-500 hover:scale-125  hover:-translate-y-10">
+      <div className=" absolute top-[13.2rem] left-5 md:top-52 md:left-96  transfom transition duration-500 hover:scale-125  hover:-translate-y-10">
       <button onClick={()=> setNav(!nav)}>
             {
                 nav ? <Message/> :null
