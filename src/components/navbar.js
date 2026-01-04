2d762a8f9031746127d8946e77139968ed96bb9c
nihar8262 <niharnics@gmail.com>
2026-01-04 23:56:09 +0530

    updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -88,22 +88,22 @@ export const Navbar = () => {
               key={link.href}
               href={link.href}
               className={`rounded-3xl p-2 transition
-                hover:bg-white/35 hover:text-yellow-500
+                hover:bg-[#631fd9]/20 hover:text-white
                 ${
                   activeSection === link.href
-                    ? "bg-white/35 text-yellow-500 font-bold"
+                    ? "bg-[#631fd9]/30 backdrop-blur-md text-white font-semibold border border-white/10 shadow-sm"
                     : ""
                 }
               `}
             >
-              {link.label}
+              <h3 className="">{link.label}</h3>
             </a>
           ))}
         </div>
         {/* Button */}
         <div className="hidden md:block">
           <a href={Bio.github} target="_blank" rel="noopener noreferrer">
-            <button class="group relative cursor-pointer outline-none border-none rounded-full flex flex-row items-center justify-center h-8 w-8 hover:!w-[100px] transition-all duration-[0.75s] before:content-[''] before:absolute before:w-full before:h-full before:inset-0 before:bg-[linear-gradient(130deg,#7209d4,#2832d4_33%,#631fd9)] before:ring-4 before:ring-offset-2 before:ring-[#631fd9] before:rounded-full before:transition before:duration-300 before:ring-offset-[#fff] hover:before:scale-105 active:before:scale-95 text-white">
+            <button class="group relative cursor-pointer outline-none border-none rounded-full flex flex-row items-center justify-start h-8 w-[100px] px-2 transition-all duration-[0.75s] before:content-[''] before:absolute before:w-full before:h-full before:inset-0 before:bg-[linear-gradient(130deg,#7209d4,#2832d4_33%,#631fd9)] before:ring-4 before:ring-offset-2 before:ring-[#631fd9] before:rounded-full before:transition before:duration-300 before:ring-offset-[#fff] hover:before:scale-105 active:before:scale-95 text-white">
               <svg
                 fill="currentColor"
                 stroke-width="0"
@@ -113,7 +113,7 @@ export const Navbar = () => {
               >
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
               </svg>
-              <span class="absolute right-2 text-[15px] font-semibold [--w:calc(100%-48px)] w-[--w] max-w-[--w] overflow-hidden flex items-center justify-end -z-[1] group-hover:z-[9] pointer-events-none select-none opacity-0 group-hover:opacity-100 text-transparent group-hover:text-inherit group-active:right-2 transition-all duration-[2s] group-hover:duration-300 group-active:scale-[0.85]">
+              <span class="absolute right-2 text-[15px] font-semibold z-20 pointer-events-none">
                 Github
               </span>
             </button>
