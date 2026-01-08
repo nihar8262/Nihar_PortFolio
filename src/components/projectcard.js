cd890a836dcd3baebc281d8b6ec478a65091a5cf
nihar8262 <niharnics@gmail.com>
2026-01-08 16:31:27 +0530

    made few changes in UI and updated some data

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -15,16 +15,16 @@ export const Projectcard = ({project}) => {
                     <div
                     className={project?.bg}>
                       <a href={project?.webapp} target='_blank' rel="noopener noreferrer">
-                      <img src={project?.image} alt="" className='hover:scale-105 transition-all ease-in-out duration-500 rounded-md mb-4 w-3/2 h-48 md:h-40 lg:h-48 xl:h-52'/>
+                      <img src={project?.image} alt="" className='hover:scale-105 transition-all ease-in-out duration-500 rounded-md mb-4 w-16/9 '/>
                       </a>
-                        <div className='flex flex-wrap gap-2 w-[16rem] text-white '>
+                        <div className='flex flex-wrap gap-2 w-full text-white '>
                      {  project.tags.map((tag)=>(
                             <h1 className='bg-gray-600/30 text-xs rounded-3xl py-1 px-1.5'>{tag}</h1>
                        ))}
                         </div>
                         <h1 className='mt-2 mb-2 text-3xl text-slate-300'>{project.title}</h1>
                         <p className='text-xs mb-2 text-slate-500 '>{project.date}</p>
-                        <p className='mb-2 text-slate-500'>
+                        <p className='mb-2 text-slate-500 text-xs md:text-sm'>
                             {project.description}
                         </p>
                         <div className='flex gap-10 mt-5'>
